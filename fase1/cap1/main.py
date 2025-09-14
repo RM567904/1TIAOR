de5d7368ee836bddbc29ffbe9ead9f901d730232
RM567904 <RM567904@fiap.com.br>
2025-09-14 14:46:00 -0300

    primeiro commit

---
+++ fase1/cap1/main.py
@@ -0,0 +1,27 @@
+'''
+a. O projeto em Python deve dar suporte a 2 tipos de culturas. O grupo vai decidir quais culturas trabalhar. Pense nas principais culturas do seu estado.
+
+b. Cálculo de área de plantio para cada cultura. O grupo decide qual tipo de figura geométrica deve-se calcular como área plantada para cada tipo de cultura;
+
+c. Cálculo do manejo de insumos. O grupo escolhe o tipo de cultura, o produto e a quantidade necessária, como por exemplo, aplicar fosfato no café e pulverizar 500 mL/metro com o trator. Quantas ruas a lavoura têm? E assim, quantos litros serão necessários?
+
+d. Os dados devem estar organizados em vetores;
+
+e. A aplicação em Python precisa ter menu de opções para:
+
+Entrada de dados (entrada dos dados para realizar os cálculos);
+Saída de dados (como impressões de dados no terminal);
+Atualização de dados numa posição qualquer do vetor;
+Deleção de dados do vetor de dados;
+Ter a opção “sair do programa”;
+f. Usar rotinas de loop e decisão.
+
+g. Na sequência, usar esses dados para desenvolver uma aplicação em R para calcular dados estatísticos básicos, como média e desvio. Deve-se usar o GitHub para versionamento do projeto, trabalhando em equipe para simular um ambiente de desenvolvimento colaborativo.
+
+h. Na disciplina de Formação Social, o grupo resumirá o artigo disponível no Google Acadêmico (https://www.alice.cnptia.embrapa.br/alice/bitstream/doc/1003485/1/CAP8.pdf). O resumo é de até 1 folha A4, letra Arial 11, espaçamento 1 entre linhas, margens direita e esquerda em 2 cm.
+
+Ir além: usando R (e não Python), conectar-se a uma API meteorológica pública para coletar dados climáticos, processar e exibir as informações meteorológicas via texto simples no terminal.
+
+'''
+
+[culturas] = []
